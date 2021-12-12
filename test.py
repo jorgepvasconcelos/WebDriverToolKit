e4779b955624aea6d7cde7363f4b67eb247ee4df
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2021-12-12 19:01:34 -0300

    feat: change to instance methods

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
 
-from webdriver_toolkit import WebDriverToolKit as wtk
+from webdriver_toolkit import WebDriverToolKit
 
 driver = Chrome()
 
-wtk.add_driver(driver=driver)
+wtk = WebDriverToolKit(driver=driver)
 
 wtk.open_site(url='https://www.google.com/')
--- webdriver_toolkit.py
+++ webdriver_toolkit.py
@@ -2,32 +2,27 @@ from selenium.webdriver.common.by import By
 
 
 class WebDriverToolKit:
-    __driver = None
+    def __init__(self, driver):
+        self.__driver = driver
 
-    @classmethod
-    def add_driver(cls, driver):
-        cls.__driver = driver
+    def add_driver(self, driver):
+        self.__driver = driver
 
-    @classmethod
-    def open_site(cls, url: str):
-        cls.__driver.get(url)
+    def open_site(self, url: str):
+        self.__driver.get(url)
 
-    @classmethod
-    def find_element_by_text(cls, text):
-        web_element = cls.__driver.find_element(By.XPATH, f"//*[contains(text(), '{text}' )]")
+    def find_element_by_text(self, text):
+        web_element = self.__driver.find_element(By.XPATH, f"//*[contains(text(), '{text}' )]")
         return web_element
 
-    @classmethod
-    def find_elements_by_text(cls, text):
-        web_elements = cls.__driver.find_elements(By.XPATH, f"//*[contains(text(), '{text}' )]")
+    def find_elements_by_text(self, text):
+        web_elements = self.__driver.find_elements(By.XPATH, f"//*[contains(text(), '{text}' )]")
         return web_elements
 
-    @classmethod
-    def find_element_by_tag_and_text(cls, tag, text):
-        web_element = cls.__driver.find_element(By.XPATH, f"//{tag}[contains(text(), '{text}' )]")
+    def find_element_by_tag_and_text(self, tag, text):
+        web_element = self.__driver.find_element(By.XPATH, f"//{tag}[contains(text(), '{text}' )]")
         return web_element
 
-    @classmethod
-    def find_elements_by_tag_and_text(cls, tag, text):
-        web_element = cls.__driver.find_elements(By.XPATH, f"//{tag}[contains(text(), '{text}' )]")
+    def find_elements_by_tag_and_text(self, tag, text):
+        web_element = self.__driver.find_elements(By.XPATH, f"//{tag}[contains(text(), '{text}' )]")
         return web_element
