22640a80705b471e74e2d0a914fe9c8e4710f6ef
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2021-12-12 19:48:07 -0300

    feat: element_is_clickable

--- test.py
+++ test.py
@@ -1,10 +1,20 @@
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
 from webdriver_toolkit import WebDriverToolKit
 
-driver = Chrome()
+options = Options()
+options.add_argument('--start-maximized')
+driver = Chrome(options=options)
 
 wtk = WebDriverToolKit(driver=driver)
 
-wtk.open_site(url='https://www.google.com/')
+driver.get('https://webscraper.io/test-sites/e-commerce/allinone/product/545')
+
+if wtk.element_is_clickable(wait_time=20, locator=(By.CSS_SELECTOR, '[class="btn swatch disabled btn-primary"]')):
+    print('é clicavel')
+else:
+    print('nao é clicavel')
+
+driver.close()
--- webdriver_toolkit.py
+++ webdriver_toolkit.py
@@ -1,4 +1,9 @@
+import traceback
+
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.common.exceptions import TimeoutException
 
 
 class WebDriverToolKit:
@@ -24,5 +29,12 @@ class WebDriverToolKit:
         return web_element
 
     def find_elements_by_tag_and_text(self, tag, text):
-        web_element = self.__driver.find_elements(By.XPATH, f"//{tag}[contains(text(), '{text}' )]")
-        return web_element
+        web_elements = self.__driver.find_elements(By.XPATH, f"//{tag}[contains(text(), '{text}' )]")
+        return web_elements
+
+    def element_is_clickable(self, wait_time, locator: tuple):
+        try:
+            WebDriverWait(self.__driver, wait_time).until(EC.element_to_be_clickable(locator))
+            return True
+        except TimeoutException:
+            return False
